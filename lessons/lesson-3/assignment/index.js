d054c19836d5cbbe75e809758ae6ec50490f0ba6
Stormwebdev <muhammadayubmamajonov425@gmail.com>
2025-04-12 10:31:55 +0500

    home

---
+++ lessons/lesson-3/assignment/index.js
@@ -0,0 +1,19 @@
+// let son1 = 2, son2 = 4;
+
+// if (son1<son2) {
+//     console.log(son2);
+//     console.log(son1);
+    
+// }
+// else{
+//     console.log(son1);
+//     console.log(son2);
+    
+// }
+
+// son1<son2 ? console.log(son2,son1) : console.log(son1,son2);
+
+
+
+
+
---
+++ lessons/lesson-3/homework/index.js
@@ -0,0 +1,39 @@
+let age = Number(prompt("Yoshingizni kiriting"))
+let data = prompt('Siz talabamisiz? (ha/yoq)')
+
+// if (age<18, data = "ha") {
+//     console.log("siz uchun chipta narxi 5000 sum");
+    
+// }else {
+//     console.log("siz uchun chipta narxi 10000 sum");
+    
+// }
+
+let chipta = (age < 18 || data.toLowerCase() === "ha") ? 5000 : 10000;
+
+console.log("Siz uchun chipta narxi: " + chipta + " so'm");
+
+
+
+// Murakkab vazifa
+
+let type = prompt("Mahsulot turini tanlang (telefon, laptop, quloqchin):")
+let soni = Number(prompt("Nechta " + type + " xarid qilmoqchisiz?"));
+let chegirma = prompt("Sizda chegirma kartasi bormi? (ha/yo'q)")
+
+
+
+
+let narx = (type === "telefon") ? 200 :
+           (type === "laptop") ? 800 :
+           (type === "quloqchin") ? 50 :
+           0;
+
+if (narx === 0) {
+    console.log(`Bu mahsulot turi mavjud emas`);
+} else {
+    let xisob = narx * soni;
+    let oxir = (chegirma === "ha" && xisob > '300$') ? xisob * 0.9 : xisob;
+
+    console.log("Yakuniy to'lov summasi: $" + oxir);
+}
