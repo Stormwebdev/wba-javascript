19f0154c4dfe1dcb836a6a02963e746744ed9a98
Stormwebdev <muhammadayubmamajonov425@gmail.com>
2025-04-19 22:06:36 +0500

    home

---
+++ lessons/lesson-6/assignment/index.js
@@ -0,0 +1,31 @@
+// function getFunction() {
+//     console.log("Name");
+//     console.log("SurName");
+//     console.log("FistName");
+//     console.log("LastName");
+//     console.log("Continue...");
+    
+// }
+
+// function getData() {
+//     // function decloration
+// }
+
+// let getInf = function() {
+//     //function expression  
+// }
+
+// let isBoolean = () => {
+//     //arrow fuction
+// }
+
+let a = 10;
+
+function getData () {
+    console.log("salom");
+    console.log(a);
+    let a = 20; 
+    
+}
+
+getData();
