0c55c0e49e4f746406dfa8b442cff6b1705fe115
Stormwebdev <muhammadayubmamajonov425@gmail.com>
2025-04-22 19:03:51 +0500

    home

---
+++ lessons/lesson-7/homework/index.js
@@ -0,0 +1,21 @@
+let student = {
+    firstName: "Eshmat",
+    lastName: "Toshmatov",
+    age: 20,
+    course: "Frontend"
+  };
+  
+  console.log(student.firstName + " " + student.lastName + " " + student.age);
+
+  
+  let data = {
+    name: "Phone",
+    model: "iPhone",
+    price: 700,
+    quantity: 8
+  };
+  
+  let allPrice = data.price * data.quantity;
+  console.log("Umumiy narx: " + allPrice);
+  
+
